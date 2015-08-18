7592f35c18a40abda5c48423a22398d497b163d6
Syed Farjad Zia Zaidi <farjad_bullet@rocketmail.com>
2015-08-18 05:54:30 +0500

    Assignment 2.3 Completed

---
+++ src/M101DotNet.WebApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace M101DotNet.WebApp.Models
+{
+    public class User
+    {
+        // XXX WORK HERE
+        // create an object suitable for insertion into the user collection
+        // The homework instructions will tell you the schema that the documents 
+        // must follow. Make sure to include Name and Email properties.
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ObjectId _id { get; set; }
+    }
+}
